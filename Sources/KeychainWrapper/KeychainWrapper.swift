8c3551041123d2726d2668488c7bd767d8a3d8da
Danny Gilbert <dgilbert@foreflight.com>
2022-07-22 17:25:23 -0500

    Initial Commit

---
+++ Sources/KeychainWrapper/KeychainWrapper.swift
@@ -0,0 +1,6 @@
+public struct KeychainWrapper {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/KeychainWrapperTests/KeychainWrapperTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import KeychainWrapper
+
+final class KeychainWrapperTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(KeychainWrapper().text, "Hello, World!")
+    }
+}
